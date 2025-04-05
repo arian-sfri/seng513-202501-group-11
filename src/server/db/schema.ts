bee6e3aace0253aa036f5a9c546b87f41ef87d33
arian-sfri <125110161+arian-sfri@users.noreply.github.com>
2025-04-04 23:21:27 -0600

    renamed folders and files in schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -6,7 +6,7 @@ export const createTable = singlestoreTableCreator(
   (name) => `cloudsync_${name}`,
 );
 
-export const files = createTable(
+export const files_table = createTable(
   "files_table", 
   {
     id: bigint("id",{mode: "number", unsigned: true}).primaryKey().autoincrement(),
@@ -19,7 +19,7 @@ export const files = createTable(
   },
 );
 
-export const folders = createTable(
+export const folders_table = createTable(
   "folders_table",
   {
     id: bigint("id",{mode: "number", unsigned: true}).primaryKey().autoincrement(),
