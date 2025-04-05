4d7254291a2bf38527c0be267f4e3bd854252a3e
arian-sfri <125110161+arian-sfri@users.noreply.github.com>
2025-04-04 23:41:07 -0600

    cleaned up data access

--- src/server/db/queries.ts
+++ src/server/db/queries.ts
@@ -1,36 +1,36 @@
+import "server-only";
+
 import { db } from "~/server/db";
 import { files_table as filesSchema, folders_table as foldersSchema } from "~/server/db/schema";
 import { eq } from "drizzle-orm";
-
-
-export async function getAllParentsForFolder(folderId: number) {
-    const parents = [];
-    let currentId: number | null = folderId;
-    while (currentId !== null) {
-        const folder = await db
-            .selectDistinct()
+export const QUERIES = {
+    getFolders: function(folderId: number){
+        return db
+            .select()
             .from(foldersSchema)
-            .where(eq(foldersSchema.id, currentId));
-        if (!folder[0]){
-            throw new Error("Parent folder not found");
+            .where(eq(foldersSchema.parent, folderId));
+    },
+    getFiles: function (folderId: number){
+        const filesPromise = db
+            .select()
+            .from(filesSchema)
+            .where(eq(filesSchema.parent, folderId));
+    },
+    getAllParentsForFolder: async function (folderId: number) {
+        const parents = [];
+        let currentId: number | null = folderId;
+        while (currentId !== null) {
+            const folder = await db
+                .selectDistinct()
+                .from(foldersSchema)
+                .where(eq(foldersSchema.id, currentId));
+            if (!folder[0]){
+                throw new Error("Parent folder not found");
 
+            }
+            parents.unshift(folder[0]);
+            currentId = folder[0]?.parent; 
         }
-        parents.unshift(folder[0]);
-        currentId = folder[0]?.parent; 
-    }
-    return parents;
-}
-
-export function getFolders(folderId: number){
-    return db
-        .select()
-        .from(foldersSchema)
-        .where(eq(foldersSchema.parent, folderId));
-}
-  
-export function getFiles(folderId: number){
-    const filesPromise = db
-        .select()
-        .from(filesSchema)
-        .where(eq(filesSchema.parent, folderId));
+        return parents;
+    },
 }
