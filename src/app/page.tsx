9efd6604e7a048808263469565ab505c3b18217c
YaseenMR <Yaaseenmrashid@gmail.com>
2025-04-20 00:41:55 -0600

    Removed the second page, goes straight to login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignInButton } from "@clerk/nextjs";
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { Button } from "~/components/ui/button";
@@ -5,7 +6,7 @@ import { Button } from "~/components/ui/button";
 export default function LandingPage() {
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-stone-50 to-stone-500 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-stone-50 to-stone-500 text-white">
       <img
         src="/images/logo.png"
         alt="Logo Watermark"
@@ -15,26 +16,19 @@ export default function LandingPage() {
         <h1 className="mb-6 text-5xl font-bold">Welcome to CloudSync</h1>
         <p className="mb-6 text-lg">Your personal cloud storage solution</p>
         <div className="mb-6 flex space-x-8">
-          <form action={async ()=>{
-            "use server";
-            
-            const session = await auth();
-            if (!session.userId) {
-              return redirect("/sign-in");
-            }
-
-            return redirect("/drive")
-          }}>
-          <Button
-          type="submit"
-          className="rounded-md bg-stone-50 px-8 py-3 text-lg font-semibold text-stone-700 transition hover:bg-stone-500 hover:text-white"
-          size="lg" 
-          >
-            Get Started
-          </Button>
-          </form>
+          <SignInButton forceRedirectUrl="/drive">
+            <Button
+              className="rounded-md bg-stone-50 px-4 py-[20px] text-lg font-semibold text-stone-700 transition hover:bg-stone-500 hover:text-white"
+              size="lg"
+            >
+              Get Started
+            </Button>
+          </SignInButton>
         </div>
       </div>
     </main>
   );
 }
+
+
+
