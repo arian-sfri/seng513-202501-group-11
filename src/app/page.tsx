21329241c1d683af17e0b238d06df2573da77635
YaseenMR <Yaaseenmrashid@gmail.com>
2025-04-21 01:03:55 -0600

    additional fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,11 @@
 import { useSignIn, useAuth } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function LandingPage() {
   const { isSignedIn } = useAuth();
-  const { signIn, isLoaded } = useSignIn()
+  const { signIn, isLoaded } = useSignIn();
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
@@ -24,17 +25,22 @@ export default function LandingPage() {
         redirectUrl: "/drive",
         redirectUrlComplete: "/drive",
       });
-    } catch (err: any) {
-      setError(err.errors?.[0]?.message || "Google sign-in failed.");
+    } catch (err) {
+      const errorMessage =
+        (err as { errors?: { message?: string }[] }).errors?.[0]?.message ||
+        "Google sign-in failed.";
+      setError(errorMessage);
     }
   };
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-br from-stone-50 to-stone-500 text-white">
-      <img
+      <Image
         src="/images/logo.png"
         alt="Logo Watermark"
         className="pointer-events-none absolute z-0 items-center opacity-10 sm:w-2/4"
+        width={500}
+        height={500}
       />
       <div className="z-10 flex flex-col items-center">
         <h1 className="mb-6 text-center text-5xl font-bold">
@@ -46,10 +52,12 @@ export default function LandingPage() {
             onClick={handleGoogleSignIn}
             className="mt-2 flex w-full items-center justify-center space-x-2 rounded-md bg-stone-200 px-4 py-2 font-semibold text-stone-600 transition hover:bg-stone-500 hover:text-stone-200"
           >
-            <img
+            <Image
               src="/images/GoogleLogo.png"
               alt="Google Logo"
               className="h-6 w-6"
+              width={24}
+              height={24}
             />
             <span>Sign in with Google</span>
           </button>
